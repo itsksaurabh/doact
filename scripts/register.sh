6be6bdd9ac1bfba3fdbe2f12fb81caa32f3499d8
itsksaurabh <itsksaurabh@gmail.com>
2020-03-17 18:16:26 +0530

    add commands to download runner

--- scripts/register.sh
+++ scripts/register.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 set -Eeuo pipefail
 #set -Eeuxo pipefail
+
+
+### Download the runner ###
+# Create a folder
+mkdir actions-runner && cd actions-runner
+# Download the latest runner package
+curl -O -L https://github.com/actions/runner/releases/download/v2.165.2/actions-runner-linux-x64-2.165.2.tar.gz
+# Extract the installer
+tar xzf ./actions-runner-linux-x64-2.165.2.tar.gz
