8e8caefe72e8e5fd92e91d6caf5fc68a2b074fef
itsksaurabh <itsksaurabh@gmail.com>
2020-03-17 18:01:46 +0530

    add main.tf template

---
+++ main.tf
@@ -0,0 +1,26 @@
+// The droplet is used as the host runner on Digital Ocean 
+// to run jobs in your GitHub Actions workflows.
+
+# Render registration.sh using a `template_file`
+data "template_file" "reg_script" {
+  template = "${file("./scripts/register.sh")}"
+}
+
+data "template_cloudinit_config" "script" {
+  gzip          = false
+  base64_encode = false
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = "${data.template_file.reg_script.rendered}"
+  }
+}
+
+resource "digitalocean_droplet" "runner" {
+  name      = "${var.project}-self-hosted-runner-${format("%02d", count.index + 1)}"
+  count     = "${var.runner_node_count}"
+  size      = "${var.node_size}"
+  image     = "${var.ubuntu}"
+  region    = "${var.region}"
+  user_data = "${data.template_cloudinit_config.script.rendered}"
+}
