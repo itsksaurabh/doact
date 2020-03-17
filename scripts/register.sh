2f7d6448d2ef55760c7d8daf1f1037041152f440
itsksaurabh <itsksaurabh@gmail.com>
2020-03-17 18:29:59 +0530

    ./scripts/register.sh: - Create a registration token using Github REST API v3 and save it to a variable - Configure the runner using config.sh

--- scripts/register.sh
+++ scripts/register.sh
@@ -2,6 +2,8 @@
 set -Eeuo pipefail
 #set -Eeuxo pipefail
 
+# Create a registration token using Github REST API v3
+REGISTRATION_TOKEN=""
 
 ### Download the runner ###
 # Create a folder
@@ -10,3 +12,7 @@ mkdir actions-runner && cd actions-runner
 curl -O -L https://github.com/actions/runner/releases/download/v2.165.2/actions-runner-linux-x64-2.165.2.tar.gz
 # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.165.2.tar.gz
+
+
+### Configure the runner ###
+./config.sh --url ${GITHUB_REPO_URL} --token $REGISTRATION_TOKEN
