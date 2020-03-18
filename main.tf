a5674d8cae1e24483767cf83430816456a8add5c
itsksaurabh <itsksaurabh@gmail.com>
2020-03-19 02:43:34 +0530

    main.tf : - Define SSH key resources to allow you to manage SSH keys for Droplet access.

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 // The droplet is used as the host runner on Digital Ocean 
 // to run jobs in your GitHub Actions workflows.
 
+// Define SSH key resources to allow you to manage SSH keys for Droplet access. 
+// Keys created with this resource can be referenced in your droplet configuration via their ID or fingerprint.
+resource "digitalocean_ssh_key" "user" {
+  name       = "${var.github_username}-rsa"
+  public_key = "${file("./ssh-keys/user_id_rsa.pub")}"
+}
+
 # Render registration.sh using a `template_file`
 data "template_file" "reg_script" {
   template = "${file("./templates/register.sh.tpl")}"
@@ -28,5 +35,6 @@ resource "digitalocean_droplet" "runner" {
   size      = "${var.node_size}"
   image     = "${var.ubuntu}"
   region    = "${var.region}"
+  ssh_keys  = ["${digitalocean_ssh_key.user.id}"]
   user_data = "${data.template_cloudinit_config.script.rendered}"
 }
