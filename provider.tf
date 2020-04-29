9a9644bcf4bbedaad2094b53a8c76ef074729679
Kumar Saurabh <kumarsaurabh@Kumars-MacBook-Air.local>
2020-04-30 00:15:04 +0530

    define provider versions

--- provider.tf
+++ provider.tf
@@ -1,4 +1,9 @@
-// Define Infrastructure provider
+// Define Infrastructure providers
 provider "digitalocean" {
   token = "${var.do_token}"
+  version = "~> v1.14.0"
+}
+
+provider "template"{
+  version = "~> 2.1"
 }
